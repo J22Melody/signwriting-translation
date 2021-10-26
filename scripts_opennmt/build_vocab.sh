7213e054740faedf7b744c5195a5d2f9d76615c1
Jiang Zifan (zifjia) <zifjia@login1>
2021-10-26 14:35:08 +0200

    fix data problem and build vocab

--- scripts_opennmt/build_vocab.sh
+++ scripts_opennmt/build_vocab.sh
@@ -4,4 +4,4 @@ scripts=`dirname "$0"`
 base=$scripts/..
 configs=$base/configs_opennmt
 
-onmt_build_vocab -config $configs/sign2en.yaml -n_sample -1
+onmt_build_vocab -config $configs/$1.yaml -n_sample -1 -overwrite
