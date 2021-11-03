c7909fa22aa96dd1baeab7a167a841c493bc2448
zifan <j22melody@gmail.com>
2021-11-03 13:13:18 +0100

    collect evaluation results

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -46,5 +46,7 @@ model=$base/models/$model_name
 # python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
 
 for test_out in $model/*.hyps.test; do	
+cat $test_out | spm_decode --model=$data/m.model > $test_out.raw
 cat $test_out | sacrebleu --lowercase --chrf-lowercase $data/test.spm.$trg -m bleu chrf > $test_out.eval
+cat $test_out.raw | sacrebleu --lowercase --chrf-lowercase $data/test.$trg -m bleu chrf > $test_out.raw.eval
 done
