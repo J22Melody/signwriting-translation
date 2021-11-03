69466e12d5b551e5b3a18d921a45f611756e6c0c
Jiang Zifan (zifjia) <zifjia@login0>
2021-11-03 12:57:05 +0100

    finish training 2factor vs. 4factor

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -23,7 +23,8 @@ model_name=$1
 model=$base/models/$model_name
 
 # CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations/test.$model_name.$trg
-python -m joeynmt translate $configs/$model_name.yaml --ckpt $model/best.ckpt < $data/test.$src > $translations/test.$model_name.$trg
+# python -m joeynmt translate $configs/$model_name.yaml --ckpt $model/best.ckpt > $translations/test.$model_name.$trg
+# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt
 
 # TODO: recasing
 # For now, compute case-insensitive BLEU by passing --lowercase to sacreBLEU
@@ -39,6 +40,11 @@ python -m joeynmt translate $configs/$model_name.yaml --ckpt $model/best.ckpt < 
 
 # cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.tokenized.$trg > $translations/test.$model_name.bleu
 # cat $translations/test.$model_name.raw.$trg | sacrebleu --lowercase $data/test.$trg > $translations/test.$model_name.raw.bleu
-cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.spm.$trg > $translations/test.$model_name.bleu
+# cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.spm.$trg > $translations/test.$model_name.bleu
 # cat $translations/test.$model_name.raw.$trg | sacrebleu --lowercase $data/test.$trg > $translations/test.$model_name.raw.bleu
-		
+
+# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
+
+for test_out in $model/*.hyps.test; do	
+cat $test_out | sacrebleu --lowercase --chrf-lowercase $data/test.spm.$trg -m bleu chrf > $test_out.eval
+done
