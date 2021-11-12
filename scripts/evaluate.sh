42333b5bde304350688076e3401145803c741a45
Jiang Zifan (zifjia) <zifjia@login1>
2021-11-12 12:44:53 +0100

    evaluate and remove baseline_transformer_spm_factor_rel/

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -43,11 +43,11 @@ model=$base/models/$model_name
 # cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.spm.$trg > $translations/test.$model_name.bleu
 # cat $translations/test.$model_name.raw.$trg | sacrebleu --lowercase $data/test.$trg > $translations/test.$model_name.raw.bleu
 
-# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
+python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
 
-for test_out in $model/*.hyps.test; do	
-cat $test_out | sacrebleu --lowercase --chrf-lowercase $data/test.spm.$trg -m bleu chrf > $test_out.eval
-# cat $test_out | spm_decode --model=$data/spm.model | $MOSES/recaser/detruecase.perl --model $data/truecase_model > $test_out.raw
-cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
-cat $test_out.raw | sacrebleu --lowercase --chrf-lowercase $data/test.$trg -m bleu chrf > $test_out.raw.eval
-done
+# for test_out in $model/*.hyps.test; do	
+# cat $test_out | sacrebleu --lowercase --chrf-lowercase $data/test.spm.$trg -m bleu chrf > $test_out.eval
+# # cat $test_out | spm_decode --model=$data/spm.model | $MOSES/recaser/detruecase.perl --model $data/truecase_model > $test_out.raw
+# cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
+# cat $test_out.raw | sacrebleu --lowercase --chrf-lowercase $data/test.$trg -m bleu chrf > $test_out.raw.eval
+# done
