7cddafce59dad95e4c23f887569c212e509a31fb
zifan <j22melody@gmail.com>
2021-11-18 16:16:39 +0100

    clean up folder

--- scripts_opennmt/build_vocab.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-scripts=`dirname "$0"`
-base=$scripts/..
-configs=$base/configs_opennmt
-
-onmt_build_vocab -config $configs/$1.yaml -n_sample -1 -overwrite
--- scripts_opennmt/train.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-scripts=`dirname "$0"`
-base=$scripts/..
-configs=$base/configs_opennmt
-
-onmt_train -config $configs/$1.yaml
