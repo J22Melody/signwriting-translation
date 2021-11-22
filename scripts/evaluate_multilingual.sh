b1a4539194f6d14e5ef22c7e78c3189654827505
zifan <j22melody@gmail.com>
2021-11-22 17:12:37 +0100

    translate script

--- scripts/evaluate_multilingual.sh
+++ scripts/evaluate_multilingual.sh
@@ -22,16 +22,18 @@ device=5
 model_name=$1
 model=$base/models/$model_name
 
-# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
+paste -d'|' $data/test.sign $data/test.sign+ $data/test.feat_col $data/test.feat_row $data/test.feat_x $data/test.feat_y $data/test.feat_x_rel $data/test.feat_y_rel \
+| python -m joeynmt translate $configs/$model_name.yaml --ckpt $model/best.ckpt \
+> $model/best1.hyps.test
 
-test_out=$model/best.hyps.test
+# test_out=$model/best.hyps.test
 
-cat $test_out | sacrebleu $data/test.spm.$trg -m bleu chrf > $test_out.eval
-cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
-cat $test_out.raw | sacrebleu $data/test.$trg -m bleu chrf > $test_out.raw.eval
+# cat $test_out | sacrebleu $data/test.spm.$trg -m bleu chrf > $test_out.eval
+# cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
+# cat $test_out.raw | sacrebleu $data/test.$trg -m bleu chrf > $test_out.raw.eval
 
-python ./scripts/split_data_by_language.py
+# python ./scripts/split_data_by_language.py
 
-for language in dict.fr en pt dict.de dict.en dict.pt; do
-    cat $test_out.raw.$language | sacrebleu $data/test.$language -m bleu chrf > $test_out.raw.$language.eval
-done
+# for language in dict.fr en pt dict.de dict.en dict.pt; do
+#     cat $test_out.raw.$language | sacrebleu $data/test.$language -m bleu chrf > $test_out.raw.$language.eval
+# done
