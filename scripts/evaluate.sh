b46da7670f9526baf072f03b4cd7ed1f4d242466
Jiang Zifan (zifjia) <zifjia@login2>
2021-11-22 13:50:38 +0100

    temporary trainging results

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -22,31 +22,10 @@ device=5
 model_name=$1
 model=$base/models/$model_name
 
-# CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.$src > $translations/test.$model_name.$trg
-# python -m joeynmt translate $configs/$model_name.yaml --ckpt $model/best.ckpt > $translations/test.$model_name.$trg
-# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt
+python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
 
-# TODO: recasing
-# For now, compute case-insensitive BLEU by passing --lowercase to sacreBLEU
-
-# cat $translations/test.$model_name.$trg | $MOSES/recaser/truecase.perl --model $MOSES/recaser/truecase.model > $translations/test.$model_name.truecased.$trg
-
-# undo tokenization
-
-# cat $translations/test.$model_name.$trg | $MOSES/tokenizer/detokenizer.perl -l $trg > $translations/test.$model_name.raw.$trg
-# cat $translations/test.$model_name.$trg | spm_decode --model=$data/m.model > translations/test.$model_name.raw.$trg
-
-# compute case-insensitive BLEU on detokenized data
-
-# cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.tokenized.$trg > $translations/test.$model_name.bleu
-# cat $translations/test.$model_name.raw.$trg | sacrebleu --lowercase $data/test.$trg > $translations/test.$model_name.raw.bleu
-# cat $translations/test.$model_name.$trg | sacrebleu --lowercase $data/test.spm.$trg > $translations/test.$model_name.bleu
-# cat $translations/test.$model_name.raw.$trg | sacrebleu --lowercase $data/test.$trg > $translations/test.$model_name.raw.bleu
-
-# python -m joeynmt test $configs/$model_name.yaml --ckpt $model/best.ckpt --output_path $model/best.hyps
-
-for test_out in $model/*.hyps.test; do	
-cat $test_out | sacrebleu $data/test.spm.$trg -m bleu chrf > $test_out.eval
-cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
-cat $test_out.raw | sacrebleu $data/test.$trg -m bleu chrf > $test_out.raw.eval
-done
+# for test_out in $model/*.hyps.test; do	
+# cat $test_out | sacrebleu $data/test.spm.$trg -m bleu chrf > $test_out.eval
+# cat $test_out | spm_decode --model=$data/spm.model > $test_out.raw
+# cat $test_out.raw | sacrebleu $data/test.$trg -m bleu chrf > $test_out.raw.eval
+# done
