8f7c5e223802496790f65613ffefcd4135306162
zifan <j22melody@gmail.com>
2021-11-25 16:33:00 +0100

    prepare data for baseline_multilingual_plus

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -13,7 +13,8 @@ def parse(raw):
     # raw: M528x518S15a07472x487S1f010490x503S26507515x483 M509x515S18720491x486 S38800464x496 M522x557S18518458x542S30d00478x483S36d01480x517S18510488x542 S38700463x496 M531x529S15a56487x517S1fb20490x495S20600509x502S36d01479x472 M515x511S10047485x490 M537x522S1f750510x507S1f758474x507S2d201511x478S2d210464x478 M541x508S17620481x492S26512459x494S17628503x492S26506526x494 M562x537S2ff00482x483S11a10547x491S11a18518x507S2c600543x462S2c611522x477 S38900464x493 M526x532S19a20498x488S22e04504x514S36d01479x469 M534x525S10030519x495S10038467x495S2a200505x476S2a218473x476 M537x518S20500497x507S20500463x507S10043507x483S2d600473x493 M551x540S14c57490x513S14c59450x498S20359530x477S20357488x461S26507519x497S26517477x483 M515x511S10047485x490 S38800464x496
     # raw: L536x584S10058480x536S26526506x570S10050498x536S20500493x571S20500526x572S36d03478x517S30a00486x483 L537x518S20500497x507S20500463x507S10043507x483S2d600473x493 L541x531S22e04524x513S15a11518x485S2ff00482x483 L538x553S14c37511x527S14c3f460x527S30d10482x488S30302482x477 S38700463x496 L566x524S10047504x503S2d60a543x503S36d03479x477 L542x524S16d51476x477S16d51522x477S26616459x508S26606501x506S26602501x493S26612459x494 L511x515S10049490x485 L518x553S20320497x447S19220497x466S11502482x489S17620497x508S11920491x527 S38700463x496 M529x530S17620513x495S1f000499x515S36d03479x471 S38700463x496 M567x527S10040523x497S2d608538x479S36d03479x473 M508x515S10e00493x485 M513x582S1f720487x419S18d20487x438S17620492x467S11a20492x487S19220492x521S1fb20493x544S14a20492x567 M532x533S20500522x522S20500498x499S22a05507x508S14210469x467 S38700463x496 M528x593S19220507x442S2a20c488x425S17620507x464S11a20507x483S10120507x517S1f720502x549S11920501x567S36d03479x407 M548x615S2ff00482x483S18710502x505S14c50521x574S14c58477x584S26510481x546S26500526x536S22520516x557S22520473x567 R526x509S14a20475x492S26606496x493 S38700463x496 M564x525S10040520x495S2d608535x477S36d03479x476 M529x538S2e74c472x513S14220498x462S2e700514x497S14228471x478 M531x511S10047469x490S2d60a508x490 M537x549S14c38474x471S14c50492x451S1f738497x517S26703464x481S1f751482x528S26707506x495 M528x523S15a06480x503S15a41473x477S23d04501x489 S38800464x496
 
-    is_valid = raw.startswith('AS') or raw.startswith('M') or raw.startswith('L') or raw.startswith('R') or raw.startswith('B')
+    is_valid = (raw.startswith('AS') or raw.startswith('M') or raw.startswith('L') or raw.startswith('R') or raw.startswith('B')) \
+        and not raw.startswith('LSM') # dirty fix for puddle id 31 "LSM Hands Group 7"
     if not is_valid:
         return False
 
@@ -113,13 +114,19 @@ for index, row in enumerate(signbank):
     # Sentences
     # en-us: Literature US, ASL Bible Books NLT, ASL Bible Books Shores Deaf Church
     # pt-br: Literatura Brasil
+    # mt-mt: Literature Malta, Literature Malta Archive
+    # de-de: Literatur DE (26, not included)
     if puddle_id == 5 or puddle_id == 151 or puddle_id == 152 or \
-       puddle_id == 114:
+       puddle_id == 114 or \
+       puddle_id == 127 or puddle_id == 147:
         # the first element is the title
         # the second is the main text
         if len(terms) > 1:
             # only take the main text
-            spoken = terms[1]
+            if puddle_id == 127 or puddle_id == 147 or puddle_id == 26:
+                spoken = terms[-1]
+            else:
+                spoken = terms[1]
 
             if puddle_id == 151:
                 # remove line-break and source, e.g., Nehemiah 3v11 NLT
@@ -127,13 +134,47 @@ for index, row in enumerate(signbank):
 
     # Dictionaries
     # en-us: Dictionary US, LLCN & SignTyp, ASL Bible Dictionary
+    # en-sg: Singapore Sign Language (SgSL) Dictionary
     # de-de: Wörterbuch DE
+    # de-ch: Wörterbuch CH-de
     # fr-ca: Dictionnaire Quebec
+    # fr-be: Dictionnaire BE-fr
+    # fr-ch: Dictionnaire CH-fr
+    # fr-fr: Dictionnaire FR
     # pt-br: Dicionário Brasil, Enciclopédia Brasil
+    # es-es: Diccionario España
+    # es-hn: Diccionario Honduras
+    # es-ar: Diccionario Argentina
+    # es-ni: Diccionario Nicaragua
+    # ca-es: Diccionario Catalán
+    # ar-tn: Dictionnaire Tunisien (French?)
+    # ko-kr: Dictionary Korea
+    # mt-mt: Dictionary Malta
+    # nl-be: Woordenboek Flanders
+    # pl-pl: Słownik PL
+    # sk-sk: Slovník CZ
+    # sl-sl: Slovar Slovenia
     if puddle_id == 4 or puddle_id == 25 or puddle_id == 28 or \
+       puddle_id == 11 or \
        puddle_id == 53 or \
+       puddle_id == 48 or \
        puddle_id == 47 or \
-       puddle_id == 46 or puddle_id == 116:
+       puddle_id == 43 or \
+       puddle_id == 49 or \
+       puddle_id == 58 or \
+       puddle_id == 46 or puddle_id == 116 or \
+       puddle_id == 55 or \
+       puddle_id == 16 or \
+       puddle_id == 41 or \
+       puddle_id == 67 or \
+       puddle_id == 56 or \
+       puddle_id == 104 or \
+       puddle_id == 78 or \
+       puddle_id == 31 or \
+       puddle_id == 44 or \
+       puddle_id == 19 or \
+       puddle_id == 52 or \
+       puddle_id == 74:
         # longest word allowed
         if len(terms[0]) < 100:
             spoken = terms[0]
--- scripts/split_data_by_language.py
+++ scripts/split_data_by_language.py
@@ -2,16 +2,16 @@ import sys
 
 source = 'sign'
 target = 'spoken'
-model_name = sys.argv[1] or 'baseline_multilingual'
+model_name = sys.argv[1]
 
-sent_languages = ['en', 'pt']
-dict_languages = ['dict.fr', 'dict.de', 'dict.en', 'dict.pt']
+sent_languages = ['en-us', 'pt-br', 'mt-mt']
+dict_languages = ['dict.en-us', 'dict.en-sg', 'dict.de-de', 'dict.de-ch', 'dict.fr-ca', 'dict.fr-be', 'dict.fr-ch', 'dict.fr-fr', 'dict.pt-br', 'dict.es-es', 'dict.es-hn', 'dict.es-ar', 'dict.es-ni', 'dict.ca-es', 'dict.ar-tn', 'dict.ko-kr', 'dict.mt-mt', 'dict.nl-be', 'dict.pl-pl', 'dict.sk-sk', 'dict.sl-sl']
 languages = sent_languages + dict_languages
 
 n_best = 5
 
 filedata = {filename: open('./data/test.{}'.format(filename), 'w+') for filename in languages}
-filedata_hyps = {filename: open('./models/{}/best.hyps.test.{}'.format(model_name, filename), 'w+') for filename in languages}
+# filedata_hyps = {filename: open('./models/{}/best.hyps.test.{}'.format(model_name, filename), 'w+') for filename in languages}
 
 with open('./data/test.{}'.format(source)) as source_file, \
      open('./data/test.{}'.format(target)) as target_file, \
@@ -23,7 +23,7 @@ with open('./data/test.{}'.format(source)) as source_file, \
 
     for index, source_line in enumerate(source_lines):
         tags = source_line.split(' ')[:3]
-        language_name = tags[0][2:4]
+        language_name = tags[0][2:4] + '-' + tags[1][2:4]
         is_dict = tags[2] == '<dict>'
         filename = 'dict.{}'.format(language_name) if is_dict else language_name
 
@@ -31,17 +31,17 @@ with open('./data/test.{}'.format(source)) as source_file, \
         file = filedata[filename]
         file.write("%s\n" % target_lines[index])
 
-        if filename in sent_languages:
-            # write the top-1 hyps for bleu/chrf
-            file_hyps = filedata_hyps[filename]
-            file_hyps.write("%s\n" % hyps_lines[index * n_best])
-        elif filename in dict_languages:
-            # write the top-n hyps for top-n accuracy
-            file_hyps = filedata_hyps[filename]
-            for i in range(n_best):
-                file_hyps.write("%s\n" % hyps_lines[index * n_best + i])
+        # if filename in sent_languages:
+        #     # write the top-1 hyps for bleu/chrf
+        #     file_hyps = filedata_hyps[filename]
+        #     file_hyps.write("%s\n" % hyps_lines[index * n_best])
+        # elif filename in dict_languages:
+        #     # write the top-n hyps for top-n accuracy
+        #     file_hyps = filedata_hyps[filename]
+        #     for i in range(n_best):
+        #         file_hyps.write("%s\n" % hyps_lines[index * n_best + i])
 
 for file in filedata.values():
     file.close()
-for file in filedata_hyps.values():
-    file.close()
+# for file in filedata_hyps.values():
+#     file.close()
