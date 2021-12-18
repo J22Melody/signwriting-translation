5f69f742bac738c8fe0b8c527f5e0053e8b5c0ff
Jiang Zifan (zifjia) <zifjia@login0>
2021-12-18 13:20:32 +0100

    training results

---
+++ scripts/sockeye_evaluate_xy.py
@@ -0,0 +1,32 @@
+import sys
+import math
+
+with open(sys.argv[1]) as file_hyps, open(sys.argv[2]) as file_gold:
+    total_distance = 0
+    total_number = 0
+
+    hyps_lines = [line.rstrip() for line in file_hyps.readlines()]
+    gold_lines = [line.rstrip() for line in file_gold.readlines()]
+
+    assert len(hyps_lines) == len(gold_lines)
+
+    for hyps_line, gold_line in zip(hyps_lines, gold_lines):
+
+        hyps_numbers = hyps_line.split(' ')
+        gold_numbers = gold_line.split(' ')
+
+        assert len(hyps_numbers) == len(gold_numbers)
+
+        total_number += len(hyps_numbers)
+
+        for hyps, gold in zip(hyps_numbers, gold_numbers):
+            distance = math.abs(int(hyps) - int(gold))
+            total_distance += distance
+
+    mean_distance = total_distance / total_number
+
+    print('total distance:', total_distance)
+    print('token number:', total_number)
+    print('mean distance:', mean_distance)
+        
+        
--- scripts/sockeye_postprocess.py
+++ scripts/sockeye_postprocess.py
@@ -4,18 +4,24 @@ model_name = sys.argv[1]
 
 with open('./models/{}/test.hyps.mixed'.format(model_name)) as source_file, \
      open('./models/{}/test.hyps.symbol'.format(model_name), 'w+') as symbol_file, \
+     open('./models/{}/test.hyps.feat_x'.format(model_name), 'w+') as x_file, \
+     open('./models/{}/test.hyps.feat_y'.format(model_name), 'w+') as y_file, \
      open('./models/{}/test.hyps.fsw'.format(model_name), 'w+') as fsw_file:
 
     source_lines = [line.rstrip() for line in source_file.readlines()]
 
     for index, source_line in enumerate(source_lines):
         symbols = []
+        xs = []
+        ys = []
         fsw_line = ''
 
         for i, item in enumerate(source_line.split(' ')):
             symbol, x, y = item.split('|')
 
             symbols.append(symbol)
+            xs.append(x)
+            ys.append(y)
 
             if symbol != 'P':
                 if i != 0:
@@ -28,4 +34,6 @@ with open('./models/{}/test.hyps.mixed'.format(model_name)) as source_file, \
                 fsw_line += y
 
         symbol_file.write("%s\n" % ' '.join(symbols))
+        x_file.write("%s\n" % ' '.join(xs))
+        y_file.write("%s\n" % ' '.join(ys))
         fsw_file.write("%s\n" % fsw_line)
